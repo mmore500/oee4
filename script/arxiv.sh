6009e062e500468db8a66337701b78dfe578604b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-05-12 17:41:48 +0900

    Purge files more aggressively

--- script/arxiv.sh
+++ script/arxiv.sh
@@ -14,15 +14,16 @@ find . -type d -name conduit -exec rm -rf {} +
 find . -type d -name web -exec rm -rf {} +
 find . -type d -name third-party -exec rm -rf {} +
 find . -type d -name docs -exec rm -rf {} +
-find . -type d -empty -delete
 find . -type l -delete
 find -type f -name ".*" -delete; rm -f *~
-rm -f **/*{.meta,.json,.ipynb,.cpp,.hpp,.jpg,.csv,@longlink}
+rm -f **/*{.meta,.json,.ipynb,.cpp,.hpp,.jpg,.csv,@longlink,.py,.md,.yaml,.jinja,.cfg}
 ls -1 submodule/dishtiny/binder/bucket=prq49/a=all_stints_all_series_profiles+endeavor=16/**/* \
     | grep -v "submodule/dishtiny/binder/bucket=prq49/a=all_stints_all_series_profiles+endeavor=16/teeplots/bucket=prq49+cat=morph+endeavor=16+transform=filter-Series-16005+viz=letterscatter-vline+x=stint+y=num-instructions+ext=" \
     | grep -v "submodule/dishtiny/binder/bucket=prq49/a=all_stints_all_series_profiles+endeavor=16/teeplots/bucket=prq49+cat=morph+endeavor=16+transform=filter-Series-16005+viz=letterscatter-vline+x=stint+y=mean-program-module-count-monoculture-mean+ext=" \
     | xargs rm -f
-rm -f submodule/**/*.png
+rm -f submodule/**/*{.png,.sh}
+rm -f submodule/**/Makefile
+find . -type d -empty -delete
 
 
 rm -f arxiv.tar.gz
