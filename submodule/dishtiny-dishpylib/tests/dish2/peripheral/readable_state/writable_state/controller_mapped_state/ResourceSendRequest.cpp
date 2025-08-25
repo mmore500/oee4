6fbb9c4785da4ca86972c0b30895f0cd50b0789a
mmore500 <mmore500.login@gmail.com>
2025-08-25 02:08:45 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_series_profiles+endeavor=16--case_study_16005.ipynb from @ mmore500/oee4@fba0685159b3f335c85b6b152f492fdc2b6596db 🚀

---
+++ submodule/dishtiny-dishpylib/tests/dish2/peripheral/readable_state/writable_state/controller_mapped_state/ResourceSendRequest.cpp
@@ -0,0 +1,7 @@
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "dish2/peripheral/readable_state/writable_state/controller_mapped_state/ResourceSendRequest.hpp"
+
+TEST_CASE("Test ResourceSendRequest") {
+  dish2::ResourceSendRequest{};
+}
