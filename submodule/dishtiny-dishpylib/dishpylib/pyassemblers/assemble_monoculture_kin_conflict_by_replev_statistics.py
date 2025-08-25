257aca88234197f15d0f1f24580823c3cb0b1185
mmore500 <mmore500.login@gmail.com>
2025-08-25 02:07:31 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_series_profiles+endeavor=16--x=fitness_complexity-phenotypic_complexity.ipynb from @ mmore500/oee4@fba0685159b3f335c85b6b152f492fdc2b6596db 🚀

---
+++ submodule/dishtiny-dishpylib/dishpylib/pyassemblers/assemble_monoculture_kin_conflict_by_replev_statistics.py
@@ -0,0 +1,54 @@
+import boto3
+import pandas as pd
+
+from dishpylib.pydecorators import announce_job_decorator_factory
+from dishpylib.pydecorators import only_every_nth_stint_decorator_factory
+from dishpylib.pydecorators import try_except_missing_data_decorator_factory
+from dishpylib.pytabulators import reshape_kin_conflict_by_replev
+
+from ._validate_assembled_dataframe import validate_assembled_dataframe
+
+@try_except_missing_data_decorator_factory(
+    'monoculture kin conflict by replev statistics'
+)
+@announce_job_decorator_factory('monoculture kin conflict by replev statistics')
+@only_every_nth_stint_decorator_factory(10)
+def assemble_monoculture_kin_conflict_by_replev_statistics(
+    *, bucket, endeavor, stint
+):
+
+    s3_handle = boto3.resource('s3')
+    bucket_handle = s3_handle.Bucket(bucket)
+
+    monoculture_kin_conflict_by_replev_statistics, \
+        = bucket_handle.objects.filter(
+            Prefix=f'endeavor={endeavor}/monocultures/metrics/stage=2+what=collated/stint={stint}/a=kin_conflict_by_replev_statistics+',
+        )
+
+    monoculture_kin_conflict_by_replev_df = pd.read_csv(
+        f's3://{bucket}/{monoculture_kin_conflict_by_replev_statistics.key}',
+    )
+
+    res_df = reshape_kin_conflict_by_replev(
+        monoculture_kin_conflict_by_replev_df
+    )
+    res_sources = [
+        monoculture_kin_conflict_by_replev_statistics.key
+    ]
+
+    # add suffix to prevent clash with monoculture dpp metrics
+    res_df.rename(
+        lambda col: \
+            col if col in {
+                'Series',
+                'Stint',
+            } \
+            else f'{col} (monoculture)' \
+        ,
+        inplace=True,
+        axis='columns',
+    )
+
+    validate_assembled_dataframe( res_df, endeavor, stint )
+
+    return res_df, res_sources
