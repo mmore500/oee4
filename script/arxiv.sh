cf9856aafb095ade38f62f1ed9ddf1b8e4b0fb92
mmore500 <mmore500.login@gmail.com>
2025-08-29 03:34:53 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_thread_profiles+endeavor=16--catalog_dataset.ipynb from @ mmore500/oee4@66166e1f8d5e31ee3ee82e3207106d4a95efb4c2 🚀

--- script/arxiv.sh
+++ script/arxiv.sh
@@ -3,28 +3,25 @@
 set -e
 shopt -s globstar
 
+for binderpath in binder*; do
+    echo "binderpath ${binderpath}"
+    find "${binderpath}" -type f ! \( -name "*.pdf" -o -name "*.tex" -o -name "*.bib" \) -exec rm -f {} +
+done
+
 find . -type f -name '*.jpg' -exec rm -f {} +
-find . -type f -name '__init__.py' -exec rm -f {} +
-find . -type d -name '*.hpp' -exec rm -rf {} +
-find . -type d -name '*.cpp' -exec rm -rf {} +
-find . -type d -name '*.json' -exec rm -rf {} +
-find . -type d -name '*.meta' -exec rm -rf {} +
-find . -type d -name '*.ipynb' -exec rm -rf {} +
+find . -type d -name dishtiny -exec rm -rf {} +
+find . -type d -name hstrat -exec rm -rf {} +
 find . -type d -name conduit -exec rm -rf {} +
-find . -type d -name web -exec rm -rf {} +
-find . -type d -name third-party -exec rm -rf {} +
 find . -type d -name docs -exec rm -rf {} +
-find . -type l -delete
-find -type f -name ".*" -delete; rm -f *~
-rm -f **/*{.meta,.json,.ipynb,.cpp,.hpp,.jpg,.csv,@longlink,.py,.md,.yaml,.jinja,.cfg}
-ls -1 submodule/dishtiny/binder/bucket=prq49/a=all_stints_all_series_profiles+endeavor=16/**/* \
-    | grep -v "submodule/dishtiny/binder/bucket=prq49/a=all_stints_all_series_profiles+endeavor=16/teeplots/bucket=prq49+cat=morph+endeavor=16+transform=filter-Series-16005+viz=letterscatter-vline+x=stint+y=num-instructions+ext=" \
-    | grep -v "submodule/dishtiny/binder/bucket=prq49/a=all_stints_all_series_profiles+endeavor=16/teeplots/bucket=prq49+cat=morph+endeavor=16+transform=filter-Series-16005+viz=letterscatter-vline+x=stint+y=mean-program-module-count-monoculture-mean+ext=" \
-    | xargs rm -f
-rm -f submodule/**/*{.png,.sh}
-rm -f submodule/**/Makefile
 find . -type d -empty -delete
 
+find ./binder -type f -name "*.pdf" -print | while read -r f; do
+    base=$(basename "$f" .pdf)   # strip .pdf extension
+    if ! grep -qF "$base" ./**/*.tex 2>/dev/null; then
+        echo rm "$f"
+        rm "$f"
+    fi
+done
 
 rm -f arxiv.tar.gz
 git checkout bibl.bib
@@ -32,6 +29,7 @@ make cleaner
 make
 make clean
 mv bibl.bib main.bib
-cp oee4-draft.bbl main.bbl
-cp oee4-draft.blg main.blg
+cp bu1.bbl main.bbl
+cp bu1.blg main.blg
+rm draft.tex
 tar -czvf arxiv.tar.gz *
