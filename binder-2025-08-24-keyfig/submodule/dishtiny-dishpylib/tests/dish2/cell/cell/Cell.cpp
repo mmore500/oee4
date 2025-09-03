bd37c0e26cf4b459d67e810a2952f17e81f082e4
mmore500 <mmore500.login@gmail.com>
2025-09-03 23:52:41 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_series_profiles+endeavor=16--x=complexity-fitness.ipynb from @ mmore500/oee4@309b3bb31121f54038b48d765e4a3e6537f67779 🚀

---
+++ binder-2025-08-24-keyfig/submodule/dishtiny-dishpylib/tests/dish2/cell/cell/Cell.cpp
@@ -0,0 +1,10 @@
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "dish2/cell/Cell.hpp"
+#include "dish2/spec/Spec.hpp"
+
+using Spec = dish2::Spec_default;
+
+TEST_CASE("Test Cell") {
+  // dish2::Cell<Spec>{};
+}
---
+++ binder-2025-08-28-complexity-adaptation/submodule/dishtiny-dishpylib/tests/dish2/spec/ResourceMeshSpec.cpp
@@ -0,0 +1,7 @@
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "dish2/spec/ResourceMeshSpec.hpp"
+
+TEST_CASE("Test ResourceMeshSpec") {
+  dish2::ResourceMeshSpec{};
+}
