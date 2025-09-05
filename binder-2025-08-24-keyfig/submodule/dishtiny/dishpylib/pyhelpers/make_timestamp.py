d5716aff4a785449d0092a28eea32d4a117a16a6
mmore500 <mmore500.login@gmail.com>
2025-09-05 22:19:37 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_thread_profiles+endeavor=16--elapsed_updates.ipynb from @ mmore500/oee4@7a40abc7b719850e21a0817634f52205f593df2d 🚀

--- binder-2025-08-24-keyfig/submodule/dishtiny/dishpylib/pyhelpers/make_timestamp.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timezone
-
-def make_timestamp():
-    return datetime.now().replace(
-        tzinfo=timezone.utc,
-    ).replace(
-        microsecond=0,
-    ).isoformat().replace('+', 'Z')
