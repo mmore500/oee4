e0298c7a2455bc4979b905f7689d62aca0bb8402
mmore500 <mmore500.login@gmail.com>
2025-09-05 22:23:15 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_series_profiles+endeavor=16--x=complexity-robustness.ipynb from @ mmore500/oee4@7a40abc7b719850e21a0817634f52205f593df2d 🚀

--- binder-2025-08-28-complexity-adaptation/submodule/dishtiny-dishpylib/postprocessing/README.md
+++
@@ -1,2 +0,0 @@
-Shell and Python scripts for post-processing data.
-Python package requirements, listed in `third-party/requirements.txt`, are pre-installed in the project's Docker container.
--- binder-2025-08-28-complexity-adaptation/submodule/dishtiny/tests/dish2/spec/ResourceMeshSpec.cpp
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN
-
-#include "Catch/single_include/catch2/catch.hpp"
-
-#include "dish2/spec/ResourceMeshSpec.hpp"
-
-TEST_CASE("Test ResourceMeshSpec") {
-  dish2::ResourceMeshSpec{};
-}
