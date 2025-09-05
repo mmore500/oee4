4ac590c1cb55c89f76aba46fd8729edcbe70aa53
mmore500 <mmore500.login@gmail.com>
2025-09-05 22:25:13 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_series_profiles+endeavor=16--x=fitness_complexity-phenotypic_complexity.ipynb from @ mmore500/oee4@7a40abc7b719850e21a0817634f52205f593df2d 🚀

--- binder-2025-08-24-keyfig/submodule/dishtiny-dishpylib/tests/dish2/peripheral/readable_state/writable_state/controller_mapped_state/ResourceSendLimit.cpp
+++
@@ -1,7 +0,0 @@
-#include "Catch/single_include/catch2/catch.hpp"
-
-#include "dish2/peripheral/readable_state/writable_state/controller_mapped_state/ResourceSendLimit.hpp"
-
-TEST_CASE("Test ResourceSendLimit") {
-  dish2::ResourceSendLimit{};
-}
--- binder-2025-08-24-keyfig/submodule/dishtiny/include/dish2/services_threadlocal/_index.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef DISH2_SERVICES_THREADLOCAL__INDEX_HPP_INCLUDE
-#define DISH2_SERVICES_THREADLOCAL__INDEX_HPP_INCLUDE
-
-#include "CellUpdateService.hpp"
-#include "DiversityMaintenanceService.hpp"
-#include "StintDiversityMaintenanceService.hpp"
-
-#endif // #ifndef DISH2_SERVICES_THREADLOCAL__INDEX_HPP_INCLUDE
