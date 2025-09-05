fa863f3cc96814c7a1acbef6aec036712838657f
mmore500 <mmore500.login@gmail.com>
2025-09-05 22:22:30 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_series_profiles+endeavor=16--x=complexity-fitness.ipynb from @ mmore500/oee4@7a40abc7b719850e21a0817634f52205f593df2d 🚀

--- binder-2025-08-28-complexity-adaptation/submodule/dishtiny-dishpylib/tests/dish2/peripheral/peripheral/Peripheral.cpp
+++
@@ -1,7 +0,0 @@
-#include "Catch/single_include/catch2/catch.hpp"
-
-#include "dish2/peripheral/Peripheral.hpp"
-
-TEST_CASE("Test Peripheral") {
-  // dish2::Peripheral{};
-}
--- binder-2025-08-28-complexity-adaptation/submodule/dishtiny/configpacks/bucket=dnh2v+mutation_rate=2.00/build_native.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-make production CXXFLAGS="-DDISH2_SPEC=dish2::Spec_dnh2v -DDISH2_NLEV=2"
