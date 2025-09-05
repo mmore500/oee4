34445450a3227bc64e684c830328710f18e6408b
mmore500 <mmore500.login@gmail.com>
2025-09-05 22:24:08 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_series_profiles+endeavor=16--mutation_accumulation.ipynb from @ mmore500/oee4@7a40abc7b719850e21a0817634f52205f593df2d 🚀

--- binder-2025-08-26-dotfig/submodule/dishtiny-dishpylib/tests/dish2/peripheral/readable_state/writable_state/controller_mapped_state/SpawnRequest.cpp
+++
@@ -1,7 +0,0 @@
-#include "Catch/single_include/catch2/catch.hpp"
-
-#include "dish2/peripheral/readable_state/writable_state/controller_mapped_state/SpawnRequest.hpp"
-
-TEST_CASE("Test SpawnRequest") {
-  dish2::SpawnRequest{};
-}
--- binder-2025-08-26-dotfig/submodule/dishtiny-dishpylib/third-party/signalgp-lite/third-party/SignalGP/README.md
+++
@@ -1,5 +0,0 @@
-# SignalGP Rimplementation Playground Repo
-
-## Testing Infrastructure
-
-- [Catch2](https://github.com/catchorg/Catch2) for native C++ testing
