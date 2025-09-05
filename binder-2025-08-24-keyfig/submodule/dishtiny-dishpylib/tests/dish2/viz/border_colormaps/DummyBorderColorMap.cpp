f9ef4fa23a507c87c834d0d8dcee56d04071ce6e
mmore500 <mmore500.login@gmail.com>
2025-09-05 22:24:33 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_series_profiles+endeavor=16--fitness.ipynb from @ mmore500/oee4@7a40abc7b719850e21a0817634f52205f593df2d 🚀

--- binder-2025-08-24-keyfig/submodule/dishtiny-dishpylib/tests/dish2/viz/border_colormaps/DummyBorderColorMap.cpp
+++
@@ -1,7 +0,0 @@
-#include "Catch/single_include/catch2/catch.hpp"
-
-#include "dish2/viz/border_colormaps/DummyBorderColorMap.hpp"
-
-TEST_CASE("Test DummyBorderColorMap") {
-  dish2::DummyBorderColorMap{};
-}
