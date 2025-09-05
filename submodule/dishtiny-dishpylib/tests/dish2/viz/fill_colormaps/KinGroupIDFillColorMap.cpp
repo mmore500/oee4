be956e0f51db735237dcba564da497e5033cd69f
mmore500 <mmore500.login@gmail.com>
2025-09-05 22:29:51 +0000

    Deploying to binder-bucket=prq49--a=all_stints_all_series_profiles+endeavor=16--x=mutation_accumulation-robustness.ipynb from @ mmore500/oee4@7a40abc7b719850e21a0817634f52205f593df2d 🚀

--- submodule/dishtiny-dishpylib/tests/dish2/viz/fill_colormaps/KinGroupIDFillColorMap.cpp
+++
@@ -1,9 +0,0 @@
-#include "Catch/single_include/catch2/catch.hpp"
-
-#include "dish2/spec/Spec.hpp"
-#include "dish2/viz/fill_colormaps/KinGroupIDFillColorMap.hpp"
-#include "dish2/viz/getters/PhylogeneticRootGetter.hpp"
-
-TEST_CASE("Test KinGroupIDFillColorMap") {
-  dish2::KinGroupIDFillColorMap{};
-}
